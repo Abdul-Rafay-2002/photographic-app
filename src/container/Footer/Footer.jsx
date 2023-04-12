8196246ec9223db39da031524ac89fd9f9d0fd6a
Abdul Rafay <rafaymalick68@gmail.com>
2023-04-12 06:33:01 +0500

    first-commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import Devlogo from '../../Assets/devlogo.png'
+import Devlogo from '../../Assets/devlogo.png';
 import '../Footer/Footer.scss';
 
 const Footer = () => {
-
-  return (
-    <>
-      <div className='footer'>
-        <p>© {new Date().getFullYear()} All Right Reserved - Photographic App By <a href='https://abdulrafayportfolio.vercel.app/' target='_blank' rel='noopener noreferrer'><img src={Devlogo} style={{ width: "65px", height: "45px" }} alt='logo' /></a></p>
-      </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div className='footer'>
+				<p>
+					© {new Date().getFullYear()} All Right Reserved - Photographic App By{' '}
+					<a
+						href='https://abdulrafayportfolio.vercel.app/'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<img
+							src={Devlogo}
+							style={{ width: '65px', height: '45px' }}
+							alt='logo'
+						/>
+					</a>
+				</p>
+			</div>
+		</>
+	);
+};
 
 export default Footer;
